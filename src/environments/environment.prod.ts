826f832c339311489de764bc158a62386c2adec4
Dagbfatih <Dagbfatih@users.noreply.github.com>
2021-08-19 21:31:54 +0300

    das

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  // apiUrl: 'https://funnytest.somee.com/api/',  
-  // baseUrl:"https://funnytest.somee.com/",
-  apiUrl:"https://localhost:44389/api/",
-  baseUrl:"https://localhost:44389/",
+  apiUrl: 'https://funnytest.somee.com/api/',  
+  baseUrl:"https://funnytest.somee.com/",
+  // apiUrl:"https://localhost:44389/api/",
+  // baseUrl:"https://localhost:44389/",
   allFieldsRequired: 'Tüm alanlar zorunludur',
   warningMessage: 'Uyarı',
   successMessage:"Başarılı",
